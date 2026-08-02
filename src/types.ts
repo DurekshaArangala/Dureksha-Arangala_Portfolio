9f7b94ca9d55e302856369f53764f3811e1f7a29
Durekshaa <durekshachammi2911@gmail.com>
2026-08-03 01:19:12 +0530

    update again

--- src/types.ts
+++ src/types.ts
@@ -6,7 +6,7 @@
 export interface Project {
   id: string;
   title: string;
-  image: String;
+  image: string;
   description: string;
   category: 'Machine Learning' | 'NLP' | 'Computer Vision' | 'Data Analytics';
   tags: string[];
